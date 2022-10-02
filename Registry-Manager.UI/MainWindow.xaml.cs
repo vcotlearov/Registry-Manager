e3791a4dc389e8157abfc8f605499e4449802cba
Vladislav Cotlearov <vladislav.kotlyarov@outlook.com>
2022-10-03 01:03:18 +0300

    Added core UI elements;

---
+++ Registry-Manager.UI/MainWindow.xaml.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Registry_Manager.UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<TabItem> _tabItems;
+        private TabItem _tabAdd;
+        private string addTabHeader =  "+";
+        public MainWindow()
+        {
+
+            try
+            {
+                InitializeComponent();
+
+                // initialize tabItem array
+                _tabItems = new List<TabItem>();
+
+                // add a tabItem with + in header 
+                TabItem tabAdd = new TabItem();
+                tabAdd.Header = addTabHeader;
+
+                _tabItems.Add(tabAdd);
+
+                // add first tab
+                this.AddTabItem();
+
+                // bind tab control
+                tabDynamic.DataContext = _tabItems;
+
+                tabDynamic.SelectedIndex = 0;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private TabItem AddTabItem()
+        {
+            int count = _tabItems.Count;
+
+            // create new tab item
+            TabItem tab = new TabItem();
+            tab.Header = string.Format("Tab {0}", count);
+            tab.Name = string.Format("tab{0}", count);
+            tab.HeaderTemplate = tabDynamic.FindResource("TabHeader") as DataTemplate;
+
+            // add controls to tab item, this case I added just a text box
+
+            RecordSelection recordSelection = new RecordSelection();
+            tab.Content = recordSelection;
+
+            // insert tab item right before the last (+) tab item
+            _tabItems.Insert(count - 1, tab);
+            return tab;
+        }
+
+        private void tabDynamic_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            TabItem tab = tabDynamic.SelectedItem as TabItem;
+
+            if (tab != null && tab.Header != null)
+            {
+                if (tab.Header.Equals(addTabHeader))
+                {
+                    // clear tab control binding
+                    tabDynamic.DataContext = null;
+
+                    // add new tab
+                    TabItem newTab = this.AddTabItem();
+
+                    // bind tab control
+                    tabDynamic.DataContext = _tabItems;
+
+                    // select newly added tab item
+                    tabDynamic.SelectedItem = newTab;
+                }
+                else
+                {
+                    // your code here...
+                }
+            }
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            string tabName = (sender as Button).CommandParameter.ToString();
+
+            var item = tabDynamic.Items.Cast<TabItem>().Where
+                       (i => i.Name.Equals(tabName)).SingleOrDefault();
+
+            TabItem tab = item as TabItem;
+
+            if (tab != null)
+            {
+                if (_tabItems.Count < 3)
+                {
+                    MessageBox.Show("Cannot remove last tab.");
+                }
+                else if (MessageBox.Show(string.Format
+                ("Are you sure you want to remove the tab '{0}'?", tab.Header.ToString()),
+                    "Remove Tab", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                {
+                    // get selected tab
+                    TabItem selectedTab = tabDynamic.SelectedItem as TabItem;
+
+                    // clear tab control binding
+                    tabDynamic.DataContext = null;
+
+                    _tabItems.Remove(tab);
+
+                    // bind tab control
+                    tabDynamic.DataContext = _tabItems;
+
+                    // select previously selected tab. if that is removed then select first tab
+                    if (selectedTab == null || selectedTab.Equals(tab))
+                    {
+                        selectedTab = _tabItems[0];
+                    }
+                    tabDynamic.SelectedItem = selectedTab;
+                }
+            }
+        }
+
+
+        private void OnExit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ Registry-Manager.UI/RecordSelection.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Registry_Manager.UI
+{
+    /// <summary>
+    /// Interaction logic for RecordSelection.xaml
+    /// </summary>
+    public partial class RecordSelection : UserControl
+    {
+        public List<RegistryRecord> registryRecords;
+        public RegistryRecord selectedRecord;
+        public RecordSelection()
+        {
+            InitializeComponent();
+
+            registryRecords = new List<RegistryRecord>();
+
+            for (int i = 0; i < 20; i++)
+            {
+                RegistryRecord registryRecord = new RegistryRecord();
+                registryRecord.Title = "Record title " + i;
+                registryRecord.RegistryKeys = new List<RegistryKey>();
+                registryRecord.RegistryKeys.Add(new RegistryKey() { Key = "LastAttemptStatus", Type="REG_DWORD", Value = "0" });
+                registryRecord.RegistryKeys.Add(new RegistryKey() { Key = "LastAttemptVersion", Type="REG_DWORD", Value = "1b8" });
+                registryRecord.RegistryKeys.Add(new RegistryKey() { Key = "LowestSupportedVersion", Type="REG_DWORD", Value = "1b8" });
+                registryRecord.RegistryKeys.Add(new RegistryKey() { Key = "Type", Type="REG_DWORD", Value = "1" });
+                registryRecord.RegistryKeys.Add(new RegistryKey() { Key = "Version", Type="REG_DWORD", Value = "1b8" });
+                registryRecords.Add(registryRecord);
+                
+                if (selectedRecord == null)
+                {
+                    selectedRecord = registryRecord;
+                }
+            }
+
+            recordsList.ItemsSource = registryRecords;
+        }
+
+        private void recordsList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            registryKeysList.ItemsSource = selectedRecord.RegistryKeys;
+        }
+    }
+    public class RegistryRecord
+    {
+        public string Title { get; set; }
+        public string Template { get; set; }
+        public List<RegistryKey> RegistryKeys { get; set; }
+    }
+
+    public class RegistryKey
+    {
+        public string Key { get; set; }
+        public string Type { get; set; }
+        public string Value { get; set; }
+    }
+}
